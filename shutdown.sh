8f686275fe040c4e27f7fa7f2a0fc60baa06abc8
leeyoshinari <lee_jc@outlook.com>
2023-12-17 21:47:09 +0800

    fix: shell script

--- shutdown.sh
+++ shutdown.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-port=$(cat config.conf | grep port | awk '{print $3}' | awk -F '\r' '{print $1}')
-pid1=$(ss -antp|grep $port |awk -F 'pid=' '{print $2 $3}'|awk -F ',' '{print $1}')
-pid2=$(ss -antp|grep $port |awk -F 'pid=' '{print $2 $3}'|awk -F ',' '{print $4}')
-if [ $pid1 ]; then
-        kill -9 $pid1
-        kill -9 $pid2
-fi
+port=$(cat config.conf | grep -E "^port" | awk -F '=' '{print $2}' | awk -F '\r' '{print $1}')
+ss -antp|grep $port |awk -F 'pid=' '{print $2 $3}'|awk -F ',' '{print $1, $4}' |xargs kill -9
 echo "Stop $port success ~"
--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ip=$(cat config.conf | grep host | awk '{print $3}' | awk -F '\r' '{print $1}')
-port=$(cat config.conf | grep port | awk '{print $3}' | awk -F '\r' '{print $1}')
-gunicorn main:app -b $ip:$port -k uvicorn.workers.UvicornWorker --daemon
+ip=$(cat config.conf | grep -E "^host" | awk -F '=' '{print $2}' | awk -F '\r' '{print $1}' | tr -d '[:space:]')
+port=$(cat config.conf | grep -E "^port" | awk -F '=' '{print $2}' | awk -F '\r' '{print $1}' | tr -d '[:space:]')
+/usr/local/python3.11.6/bin/gunicorn main:app -b $ip:$port -k uvicorn.workers.UvicornWorker --timeout 30 --daemon
 echo "start server success ~"
